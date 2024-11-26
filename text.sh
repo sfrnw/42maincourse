6cfc12a7b43467e62730cb06b5c5ca9335370436
Aleksandr Safronov <asafrono@student.42lisboa.com>
2024-11-26 11:51:06 +0000

    test

---
+++ text.sh
@@ -0,0 +1,17 @@
+git filter-branch --env-filter '
+OLD_EMAIL="[email protected]"
+CORRECT_NAME="Aleksandr Safronov"
+CORRECT_EMAIL="asafrono@student.42lisboa.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
